552fb649fd6281e1184c301411696103a9963576
lestherxm <lestherxm@gmail.com>
2022-10-13 00:18:14 -0600

    modify readme file

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # pern stack TASKS
+
+# How can I try this?
+- Clone the repository
+- Run `Docker desktop`
+- Open the project in your code editor
+- Run in terminal this to start PostgreSQL service (in case you don't have it yet)
+```
+docker-compose up
+```
+- Finally, start the server by running
+```
+npm start
+```
+- Test the API endpoints using Postman or your favorite client API tester
--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ const port = 3000;
 app.use(cors()); //comunicar ambos servers de manera simple (front y back)
 app.use(morgan('dev')); //ver por consola las peticiones a la API
 app.use(express.json()); //express server no entiende JSON nativamente, es necesario importar un metodo para que si lo haga.
+
 app.use(tastkRoutes);
+
 //Next() function para manejar errores en los controladores a través de una sola funcion
 app.use((err, req, res, next) =>{
     return res.json({
